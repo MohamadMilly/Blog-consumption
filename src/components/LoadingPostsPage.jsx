e40e95505bdc83a4fd019516c79b2e5bc510b38c
MohamadMilly <volassoa@gmail.com>
2026-01-04 10:47:13 +0300

    duplicate the post skeleton

--- src/components/LoadingPostsPage.jsx
+++ src/components/LoadingPostsPage.jsx
@@ -21,6 +21,86 @@ export function LoadingPostsPage() {
           </div>
         </div>
       </article>
+      <article className="flex flex-col ">
+        <div className=" mb-2 flex justify-between items-center flex-wrap gap-y-2 pb-2 border-b border-gray-400/20">
+          <div className="flex items-center">
+            <div className="w-8 h-8 mr-2 rounded-full bg-gray-600/20 animate-pulse"></div>
+            <span className="block w-20 py-1 bg-gray-600/20 animate-pulse rounded"></span>
+          </div>
+          <span className="block w-15 py-1 bg-gray-600/20 animate-pulse rounded"></span>
+        </div>
+        <div className="flex flex-col min-h-80 sm:h-60 sm:min-h-0  sm:flex-row gap-4 bg-gray-800/15 rounded-lg overflow-hidden mb-4 border-2 border-gray-800/10 border-dotted">
+          <div className="w-full h-40 sm:h-full sm:w-50 shrink-0 flex items-center justify-center overflow-hidden bg-gray-600/20 animate-pulse"></div>
+          <div className="flex flex-col grow px-4 py-2">
+            <h2 className="py-2 rounded bg-gray-600/20 animate-pulse mb-4"></h2>
+            <p className="bg-gray-600/20 py-6 my-2 rounded animate-pulse"></p>
+
+            <div className="mt-auto">
+              <button className="w-full  px-3 py-2 bg-gray-600/20 rounded animate-pulse"></button>
+            </div>
+          </div>
+        </div>
+      </article>
+      <article className="flex flex-col ">
+        <div className=" mb-2 flex justify-between items-center flex-wrap gap-y-2 pb-2 border-b border-gray-400/20">
+          <div className="flex items-center">
+            <div className="w-8 h-8 mr-2 rounded-full bg-gray-600/20 animate-pulse"></div>
+            <span className="block w-20 py-1 bg-gray-600/20 animate-pulse rounded"></span>
+          </div>
+          <span className="block w-15 py-1 bg-gray-600/20 animate-pulse rounded"></span>
+        </div>
+        <div className="flex flex-col min-h-80 sm:h-60 sm:min-h-0  sm:flex-row gap-4 bg-gray-800/15 rounded-lg overflow-hidden mb-4 border-2 border-gray-800/10 border-dotted">
+          <div className="w-full h-40 sm:h-full sm:w-50 shrink-0 flex items-center justify-center overflow-hidden bg-gray-600/20 animate-pulse"></div>
+          <div className="flex flex-col grow px-4 py-2">
+            <h2 className="py-2 rounded bg-gray-600/20 animate-pulse mb-4"></h2>
+            <p className="bg-gray-600/20 py-6 my-2 rounded animate-pulse"></p>
+
+            <div className="mt-auto">
+              <button className="w-full  px-3 py-2 bg-gray-600/20 rounded animate-pulse"></button>
+            </div>
+          </div>
+        </div>
+      </article>
+      <article className="flex flex-col ">
+        <div className=" mb-2 flex justify-between items-center flex-wrap gap-y-2 pb-2 border-b border-gray-400/20">
+          <div className="flex items-center">
+            <div className="w-8 h-8 mr-2 rounded-full bg-gray-600/20 animate-pulse"></div>
+            <span className="block w-20 py-1 bg-gray-600/20 animate-pulse rounded"></span>
+          </div>
+          <span className="block w-15 py-1 bg-gray-600/20 animate-pulse rounded"></span>
+        </div>
+        <div className="flex flex-col min-h-80 sm:h-60 sm:min-h-0  sm:flex-row gap-4 bg-gray-800/15 rounded-lg overflow-hidden mb-4 border-2 border-gray-800/10 border-dotted">
+          <div className="w-full h-40 sm:h-full sm:w-50 shrink-0 flex items-center justify-center overflow-hidden bg-gray-600/20 animate-pulse"></div>
+          <div className="flex flex-col grow px-4 py-2">
+            <h2 className="py-2 rounded bg-gray-600/20 animate-pulse mb-4"></h2>
+            <p className="bg-gray-600/20 py-6 my-2 rounded animate-pulse"></p>
+
+            <div className="mt-auto">
+              <button className="w-full  px-3 py-2 bg-gray-600/20 rounded animate-pulse"></button>
+            </div>
+          </div>
+        </div>
+      </article>
+      <article className="flex flex-col ">
+        <div className=" mb-2 flex justify-between items-center flex-wrap gap-y-2 pb-2 border-b border-gray-400/20">
+          <div className="flex items-center">
+            <div className="w-8 h-8 mr-2 rounded-full bg-gray-600/20 animate-pulse"></div>
+            <span className="block w-20 py-1 bg-gray-600/20 animate-pulse rounded"></span>
+          </div>
+          <span className="block w-15 py-1 bg-gray-600/20 animate-pulse rounded"></span>
+        </div>
+        <div className="flex flex-col min-h-80 sm:h-60 sm:min-h-0  sm:flex-row gap-4 bg-gray-800/15 rounded-lg overflow-hidden mb-4 border-2 border-gray-800/10 border-dotted">
+          <div className="w-full h-40 sm:h-full sm:w-50 shrink-0 flex items-center justify-center overflow-hidden bg-gray-600/20 animate-pulse"></div>
+          <div className="flex flex-col grow px-4 py-2">
+            <h2 className="py-2 rounded bg-gray-600/20 animate-pulse mb-4"></h2>
+            <p className="bg-gray-600/20 py-6 my-2 rounded animate-pulse"></p>
+
+            <div className="mt-auto">
+              <button className="w-full  px-3 py-2 bg-gray-600/20 rounded animate-pulse"></button>
+            </div>
+          </div>
+        </div>
+      </article>
     </section>
   );
 }
